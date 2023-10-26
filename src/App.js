dc653684243a6f1b93264302d890ff0ff44c56ef
Artiom Nazarchuk <sel_it@tut.by>
2023-10-26 15:16:42 +0300

    Start Project React_App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// import Counter from "./components/Counter";
+// import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [posts, /*setPosts*/] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'},
+    ])
 
+
+    return (
+        <div className="App">
+            {posts.map(post =>
+                <PostItem post={post} key={post.id}/>
+            )}
+        </div>
+    );
+}
 export default App;
